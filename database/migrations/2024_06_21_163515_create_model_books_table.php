b50e113e38825aebd613a0ae59f387082dc51e40
teresabat <contatotommymateus@gmail.com>
2024-06-21 18:05:20 -0300

    First Commit!

---
+++ database/migrations/2024_06_21_163515_create_model_books_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_user')->unsigned();
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book');
+    }
+};
---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('templates.template')
+
+@section('content')
+    <h1 class="text-center">Crud</h1>
+
+@endsection
